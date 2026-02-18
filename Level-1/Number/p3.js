a4b83f7039b00b951166030cf84d9fd443fb6292
Khalid <khalidanwarantur@gmail.com>
2026-02-18 22:47:56 +0600

    done

--- Level-1/Number/p3.js
+++ Level-1/Number/p3.js
@@ -8,3 +8,16 @@ function fibonaki(n){
     return array
 }
 console.log(fibonaki(10))
+
+
+function fibo(n){
+    if(n===1) return [0]
+    if(n===2) return [0,1]
+    let arr = [0,1]
+    for(let i = 2;i<n;i++){
+        arr[i]=arr[i-1]+ arr[i-2]
+    }
+    return arr
+}
+
+console.log(fibo(50))
