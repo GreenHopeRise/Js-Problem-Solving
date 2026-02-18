938fff14c7db3de63896edfbb029f8e3e0c7ff76
Khalid <khalidanwarantur@gmail.com>
2026-02-18 23:21:01 +0600

    done

--- Level-1/Number/p3.js
+++ Level-1/Number/p3.js
@@ -12,12 +12,12 @@ console.log(fibonaki(10))
 
 function fibo(n){
     if(n===1) return [0]
-    if(n===2) return [0,1]
-    let arr = [0,1]
+    if(n===2) return [5,5]
+    let arr = [5,5]
     for(let i = 2;i<n;i++){
         arr[i]=arr[i-1]+ arr[i-2]
     }
     return arr
 }
 
-console.log(fibo(50))
+console.log(fibo(5))
---
+++ Level-1/Number/p5.js
@@ -0,0 +1,6 @@
+function evenOrOdd(n){
+    if(n%2===0)return true
+    if(n%2!==0)return false
+}
+
+console.log(evenOrOdd(5))
