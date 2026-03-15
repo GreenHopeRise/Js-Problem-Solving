7496fbd69ca5fc91fe73390a66a18971a2c2f79b
GreenHopeRise <khalidanwarantur96@gmail.com>
2026-03-15 08:37:35 +0600

    vowel done

--- Level-1/String/p3.js
+++ Level-1/String/p3.js
@@ -22,3 +22,5 @@ function vowel(str){
 }
 
 console.log(vowel('ami vhalo nai'))
+
+
--- Level-1/String/t2/pt1.js
+++ Level-1/String/t2/pt1.js
@@ -22,3 +22,32 @@ function palindrome(x){
     else return 'not palindrome'
 }
 console.log(palindrome('mad'))
+
+function palindromeUpdate(x){
+    let first = 0
+    let last = x.length-1
+    while(first<last){
+        if(x[first]!==x[last]){
+            return 'not palindrome'
+        }
+        first++
+        last--
+    }
+    return 'palindrome'
+}
+
+console.log(palindromeUpdate('madam'))
+
+
+// vowle counter
+function vowleCounter(x){
+    let count = 0
+    for(let i = 0; i<x.length; i++){
+        if(x[i]===' ')continue
+        if(x[i]=== 'a'||x[i]=== 'e'||x[i]=== 'i'||x[i]=== 'o'||x[i]=== 'u'||x[i]=== 'A'||x[i]=== 'E'||x[i]=== 'I'||x[i]=== 'O'||x[i]=== 'U'){
+            count=count+1
+        }
+    }
+    return count
+}
+console.log(vowleCounter('ami onek vhalo achi'))
